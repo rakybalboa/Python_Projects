79489d9f6bdd6ab68c11008186dfa9795278d62b
Ramazan BASTURK <basturkramazan@gmail.com>
2023-09-12 22:21:10 +0300

    Some Changes on time format and background items

--- Digital Clock/Digital_Clock.py
+++ Digital Clock/Digital_Clock.py
@@ -16,19 +16,20 @@ from time import strftime
 
 # creating tkinter window
 root = Tk()
-root.title('Amar\'s Clock')
+root.title('Raky\'s Clock')
+root.configure(bg='blue')
 
 
 # This function is used to display time on the label
 def time():
-    string = strftime('%I:%M:%S %p')
+    string = strftime('%H:%M:%S %p UTC %z')
     # To make the clock format 24 hrs. Simply change the strftime('%I:%M:%S %p') to strftime('%H:%M:%S %p').
     lbl.config(text=string)
     lbl.after(1000, time)  # It will update the time after every (1000 milliseconds i.e. after 1 second)
 
 
 # Styling the label widget so that clock will look more attractive
-lbl = Label(root, font=('calibri', 40, 'bold'), background='purple', foreground='white')
+lbl = Label(root, font=('times-new-roman', 30, 'bold'), background='skyblue', foreground='white')
 
 
 # Placing clock at the centre of the tkinter window
